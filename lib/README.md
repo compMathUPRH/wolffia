3002c6e440fd320f8d26e28ff525165c43596f43
José O. Sotero Esteva <jose.sotero@upr.edu>
2020-05-26 13:31:41 -0400

    Preparativos para branch wolffialib

---
+++ lib/README.md
@@ -0,0 +1,5 @@
+# wolffia-pkg
+
+A set of modules to setup and analyze molecular dynamics simulations.
+[Github-flavored Markdown](https://github.com/compMathUPRH/wolffia/)
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wolffialib",
+    version="0.0.1",
+    author="José O. Sotero Esteva et. al.",
+    author_email="jose.sotero@upr.edu",
+    description="A set of modules to setup and analyze molecular dynamics simulations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/compMathUPRH/wolffia",
+    packages=setuptools.find_packages(),
+    classifiers=[
+	"Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+	"Intended Audience :: Education",
+	"Intended Audience :: Science/Research",
+	"Topic :: Scientific/Engineering :: Chemistry",
+    ],
+    keywords='molecular dynamics',
+    install_requires=['matplotlib', 'numpy', 'networkx>=2.4'],
+    python_requires='>=3.6',
+)
